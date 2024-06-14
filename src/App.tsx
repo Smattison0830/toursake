8e5e4201502b49226eba2984e86dc8a1aa8ff5ab
Spencer Mattison <spenceramattison@gmail.com>
2024-06-14 14:39:03 +0900

    Created pages for navbar and updated router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import Map from './components/Map';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Map />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 };
