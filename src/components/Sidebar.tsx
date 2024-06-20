e62798c64157bc7d9146b95d971e3085ead10c10
Spencer Mattison <spenceramattison@gmail.com>
2024-06-20 15:55:28 +0900

    Added icons to sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import whiskeyIcon from '../assets/whiskey.svg';
+import sakeIcon from '../assets/sake.svg';
+import beerIcon from '../assets/beer.svg';
 
 const Sidebar: React.FC = () => {
-  return (
-    <div className="fixed top-24 left-10 w-1/8 bg-orange-500 bg-opacity-50 rounded-lg p-4 z-10">
-      <h2 className="text-sm mb-4"></h2>
-      <ul>
-        <li className="mb-2">Whiskey</li>
-        <li className="mb-2">Beer</li>
-        <li className="mb-2">Sake</li>
-      </ul>
-    </div>
-  );
+    return (
+        <div className="fixed top-24 left-10 w-1/8 bg-orange-500 bg-opacity-50 rounded-lg p-4 z-10">
+         <h2 className="text-lg font-bold mb-4"></h2>
+          <ul>
+            <li className="mb-2 flex items-center cursor-pointer">
+              <img src={whiskeyIcon} alt="Whiskey" className="w-6 h-6 mr-2" />
+            </li>
+            <li className="mb-2 flex items-center cursor-pointer">
+              <img src={sakeIcon} alt="Sake" className="w-6 h-6 mr-2" />
+            </li>
+            <li className="mb-2 flex items-center cursor-pointer">
+              <img src={beerIcon} alt="Beer" className="w-6 h-6 mr-2" />
+            </li>
+          </ul>
+        </div>
+      );
 };
 
 export default Sidebar;
