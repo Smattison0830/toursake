d5ed481658c47b05347c394651d48371c8575475
Spencer Mattison <spenceramattison@gmail.com>
2024-06-26 14:01:12 +0900

    Removed text in Locations.tsx and added component

--- src/pages/Locations.tsx
+++ src/pages/Locations.tsx
@@ -1,37 +1,12 @@
 import React from 'react';
+import AllLocations from '../components/Locations'; // Import the new component
 
 const Locations: React.FC = () => {
 
   return (
     <div className='fixed w-full flex justify-center mt-4 z-10 top-20 bottom-4'>
       <div className=" bg-orange-500 bg-opacity-80 rounded-lg w-11/12 p-4 left-0 right-0 overflow-y-scroll">
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
-        <h1 className="text-center text-3xl">Locations Page</h1>
+        <AllLocations />
       </div>
     </div>
   );
